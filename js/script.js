32047412100a3bc65a899bbada7f688df381c303
Куроптев Д.Б. <d.kuroptev@kto.Ges.local>
2024-07-19 11:59:12 +0300

    add reviews section

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper(".swiper", {
+  parallax: true,
+  autoplay: {
+    delay: 7000,
+  },
+  loop: true,
+  autoHeight: true,
+  watchSlidesProgress: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      autoplay: false
+    },
+    
+    992: {
+      autoplay: {
+        delay: 7000,
+      },
+    }
+  }
+});
