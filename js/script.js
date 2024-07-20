62b5f300c9e8d2c669c6b927255a5bdee145bcb3
kuroptevdb <kuroptevdb@gmail.com>
2024-07-20 17:30:59 +0300

    add menu hambuger and scroll to sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+const hamburgerBtn = document.querySelector(".hamburger");
+const closeNavMenuBtn = document.querySelector(".nav-menu__close");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburgerBtn.addEventListener("click", () => {
+  navMenu.classList.add("nav-menu_active");
+});
+
+closeNavMenuBtn.addEventListener("click", () => {
+  navMenu.classList.remove("nav-menu_active");
+});
+
 const swiper = new Swiper(".swiper", {
   parallax: true,
   autoplay: {
@@ -14,15 +26,31 @@ const swiper = new Swiper(".swiper", {
 
   breakpoints: {
     320: {
-      autoplay: false
+      autoplay: false,
     },
-    
+
     992: {
       autoplay: {
         delay: 7000,
       },
-    }
-  }
+    },
+  },
 });
+
+function toSection(nameSection) {
+  const section = document.querySelector(`#${nameSection}`);
+  const link = document.querySelector(`[href="#${nameSection}"]`);
+
+
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    section.scrollIntoView({ behavior: "smooth" });
+    navMenu.classList.remove("nav-menu_active");
+  });
+}
+
+toSection("our-menu");
+toSection("reserve");
+toSection("history");
 
 new WOW().init();
